88939e278db57196a2d743880357b3b81b40f943
Noroboto <danieldiazg01@gmail.com>
2024-07-29 23:00:55 -0400

    feat: expo lint plugin

--- examples/reactNative/.eslintrc.js
+++ examples/reactNative/.eslintrc.js
@@ -17,10 +17,6 @@ module.exports = {
       typescript: {
         project: __dirname + "/tsconfig.json",
       },
-      alias: {
-        map: [["@", "./"]],
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-      },
     },
   },
 };
